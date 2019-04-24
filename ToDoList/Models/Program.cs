2ff2636d960e105e106dceb2cb275633089c5c37
Brian Hensley and Reese Lee <brian@>
2019-04-24 11:46:21 -0700

    we may not have made any progress at all....

--- ToDoList/Models/Program.cs
+++ ToDoList/Models/Program.cs
@@ -9,36 +9,35 @@ namespace ToDoList.Models
     {
       Console.WriteLine("Would you like to add a list item? [y]es, [n]o, or [v]iew list");
       string userResponse = Console.ReadLine().ToLower();
+
+      //give thks a class
+
       if (userResponse == "y")
       {
         Console.WriteLine("Ay ay ay. You wan a list?");
         string userAddItem = Console.ReadLine();
         Item newUserListItem = new Item(userAddItem);
         Main();
+        Console.WriteLine(newUserListItem.GetDescription());
       }
+
       else if (userResponse == "n")
       {
-
+        Console.WriteLine("HELLO");
+        return;
       }
+
       else if (userResponse == "v")
       {
         //foreach loop
         // Console.WriteLine(newUserListItem.GetDescription());
       }
+
       else
-      {}
+      {
+      }
 
-
-
-
-
-
-      // LeetSpeak testLeetSpeak = new LeetSpeak();
-      // Console.WriteLine("3nt3r 73x7 7o 1337 7ha7 sh17");
-      // string userInput = Console.ReadLine();
-      // // char[] testArray = {'a', 'b', ' ', 'c'};
-      // string result = testLeetSpeak.LeetSpeakConverter(userInput);
-      // Console.WriteLine(result);
+        // End class
     }
   }
 }
