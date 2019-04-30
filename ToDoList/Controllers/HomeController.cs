920966f4cabba08fc8d2fb07a685509120cfbf9b
Brian Hensley and Jared Farkas <brian@>
2019-04-30 09:22:37 -0700

    implement tests

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,29 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
+using System.Collections.Generic;
 
 namespace ToDoList.Controllers
 {
   public class HomeController : Controller
   {
 
-    [Route("/")]
+    [HttpGet("/")]
     public ActionResult Index()
     {
-      Item starterItem = new Item("Add first item to To Do List");
-      return View(starterItem);
-    }
-
-    [Route("/items/new")]
-    public ActionResult CreateForm()
-    {
       return View();
-    }
-
-    [Route("/items")]
-    public ActionResult Create(string description)
-    {
-      Item myItem = new Item(description);
-      return View("Index", myItem);
     }
 
   }
--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -25,6 +25,7 @@ namespace ToDoList
 
     public void Configure(IApplicationBuilder app)
     {
+      app.UseDeveloperExceptionPage();
       app.UseMvc(routes =>
       {
         routes.MapRoute(
